bc0756f8f5955c7996d6375c92f3560dcf28f2a3
spyware293 <faisalfiqhi@gmail.com>
2017-01-12 04:57:48 +0800

    Add Color option
    Add Color option

--- Skill Locator/Skill Locator/Program.cs
+++ Skill Locator/Skill Locator/Program.cs
@@ -17,6 +17,19 @@ namespace Skill_Locator
         private static Font font;
         private static readonly Menu Menu = new Menu("SkillLocator", "SkillLocator", true);
         private static List<Vector2> pos = new List<Vector2>();
+        static Dictionary<string, Color> colorselect = new Dictionary<string, Color>()
+        {
+            {"Blue",Color.Blue },
+            {"Teal",Color.Teal },
+            {"Purple",Color.Purple },
+            {"Yellow",Color.Yellow },
+            {"Orange",Color.Orange },
+            {"Pink",Color.Pink },
+            {"Gray",Color.Gray },
+            {"Light Blue",Color.LightBlue },
+            {"Green",Color.Green },
+            {"Brown",Color.Brown }
+        };
         public static List<Vector2> Pos
         {
             get { return pos; }
@@ -24,6 +37,7 @@ namespace Skill_Locator
         }
         static void Main(string[] args)
         {
+            Menu.AddItem(new MenuItem("color", "Marker Color").SetValue(new StringList(new[] { "Blue", "Teal", "Purple", "Yellow","Orange","Pink","Gray","Light Blue","Green","Brown" })));
             Menu.AddItem(new MenuItem("delay", "Delay before mark gone").SetValue(new Slider(2000, 1000, 5000)));
             Menu.AddToMainMenu();
             font = new Font(
@@ -47,14 +61,15 @@ namespace Skill_Locator
         {
             font.OnLostDevice();
         }
-        private static async void draw(EventArgs args)
+        private static void draw(EventArgs args)
         {
             //  
             try
             {
                 foreach (Vector2 position in Pos.ToList())
                 {
-                    font.DrawText(null, "+", (int)position.X - 13, (int)position.Y - 10, Color.Red);
+                    
+                    font.DrawText(null, "+", (int)position.X - 13, (int)position.Y - 10, colorselect[Menu.Item("color").GetValue<StringList>().SelectedValue]);
                 }
                 
             }
@@ -75,7 +90,7 @@ namespace Skill_Locator
             {
                 if (!args.Entity.Name.Contains("npc_dota_creep") && !args.Entity.Name.Contains("npc_dota_neutral") && !args.Entity.Name.Contains("npc_dota_badguys") && !args.Entity.Name.Contains("npc_dota_goodguys") && !args.Entity.Name.Contains("dota_scene_entity") && !args.Entity.Name.Contains("point_hmd") && !args.Entity.Name.Contains("dota_item") && !args.Entity.Name.Contains("ent_dota_tree") && !args.Entity.Name.Contains("dota_world") && !args.Entity.Name.Contains("ambient_") && args.Entity.NetworkPosition.X != 0 && !args.Entity.Name.Contains("npc_dota_hero_") && !args.Entity.Name.Contains("effigy") && !args.Entity.Name.Contains("prop_dynamic") && !args.Entity.Name.Contains("world_layer") && !args.Entity.NetworkName.Contains("Spawner") && !args.Entity.Name.Contains("dota_shop") && !args.Entity.Name.Contains("fountain") && !args.Entity.Name.Contains("halloffame") && !args.Entity.Name.Contains("info_player") && !args.Entity.Name.Contains("info_particle") && !args.Entity.Name.Contains("Camera") && !args.Entity.Name.Contains("tonemap") && args.Entity.NetworkName != "CDOTAPlayer" && !args.Entity.Owner.IsVisible)
                 {
-                   // Game.PrintMessage(args.Entity.Name + " " + args.Entity.NetworkName + " " + args.Entity.ClassID);
+                   // Game.PrintMessage(args.Entity.Name + " " + args.Entity.NetworkName + " " + args.En);
                     minimap_pos2d = HUDInfo.WorldToMinimap(args.Entity.NetworkPosition);
                     Pos.Add(minimap_pos2d);
                     //  Game.PrintMessage(minimap_pos2d.X + " " + minimap_pos2d.Y);
